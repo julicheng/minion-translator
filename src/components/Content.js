4d55824275dbff7eacdbe23d38d436c430581ee3
Juli Cheng <julicheng@Julis-MacBook-Pro.local>
2019-12-13 16:28:03 +0000

    Add the remaining boilerplate

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Image from "./Image";
+import Translation from "./Translation";
+import TextInput from "./TextInput";
 
 const Content = () => {
-  return <h1>Content</h1>;
+  return (
+    <>
+      <Image />
+      <Translation />
+      <TextInput />
+    </>
+  );
 };
 
 export default Content;
---
+++ src/components/Translation.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Translation = () => {
+  return <h2>translation</h2>;
+};
+
+export default Translation;
