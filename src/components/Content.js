6857ebc4baf64ee59f688a75aeaec2bb5971cf77
Juli Cheng <julicheng@Julis-MacBook-Pro.local>
2019-12-13 16:33:41 +0000

    Add context boilerplater

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,14 +2,15 @@ import React from "react";
 import Image from "./Image";
 import Translation from "./Translation";
 import TextInput from "./TextInput";
+import TranslationContextProvier from "../contexts/TranslationContext";
 
 const Content = () => {
   return (
-    <>
+    <TranslationContextProvier>
       <Image />
       <Translation />
       <TextInput />
-    </>
+    </TranslationContextProvier>
   );
 };
 
---
+++ src/contexts/TranslationContext.js
@@ -0,0 +1,7 @@
+import React, { createContext } from "react";
+
+export const TranslationContext = createContext();
+
+const TranslationContextProvier = props => {};
+
+export default TranslationContextProvier;
